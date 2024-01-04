381dec1b172a8aefb4b99d76858a3cea01608c83
xvanstaen <xvanstaen@gmail.com>
2024-01-04 16:18:41 +0800

    a

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ mongoConfig.getConfigServer()
 
 
 function displayStatus(code){
-  const myVersion="Version 02Jan2024 V0-1";
+  const myVersion="Version 04Jan2024 V0-1";
   var configData = "";
   if (code===200){
     configData=" configData retrieved in MongoDB";
